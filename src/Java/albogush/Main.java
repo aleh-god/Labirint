f31b81955e80e2266403be8cb6d71eb1f39947a4
aleh-god <aleh.god@gmail.com>
2020-11-18 19:47:02 +0300

    Start
    Все удалили, объединили и закачали заново.

--- src/Java/albogush/Main.java
+++ src/Java/albogush/Main.java
@@ -4,37 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int poleHeight = 50;
-        int poleLength = 50;
-        Integer PL = new Integer(poleHeight);
+        int poleHeight = 20;
+        int poleLength = 20;
+
         //Random.nextInt(26)
         char[][] pole = new char[poleLength][poleHeight];
 
         //Рисуем пустое поле
         for (int i = 0; i < poleLength; i++){
             for(int j=0; j < poleHeight; j++){
-               if (i != poleLength && i != 0){
+                // Рисуем 0 если (i не равно 0 или 49) или (j не равно 0 или 49)
+                // Общее выражение true - рисуем 0
+                if (( (i == 0) | (i == (poleLength-1) ) | ( (j == 0) | (j == (poleHeight-1)) ))){
                    pole[i][j] = '*';}
-               else{
-                   pole[i][j] = '0';
-               };
-
-                switch(j){
-                    case 0:
-                    case PL:
-                        pole[i][j] = '*';
-                        break;
-                    default:
-                        pole[i][j] = ' ';
-                }
+                else{
+                   pole[i][j] = ' ';
+                };
             }
-        }
-        // Рисуем рамку
-        for (int i = 0; i < pole.length; i++){
-            pole[i][0] = '*';
-            pole[i][49] = '*';
-            pole[0][i] = '*';
-            pole[49][i] = '*';
         }
 
         drawPole(pole);
